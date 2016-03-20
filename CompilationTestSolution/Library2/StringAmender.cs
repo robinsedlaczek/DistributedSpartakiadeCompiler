8eff09f4f9ebb6e2054039e5db0f4f27be580c58
Philip Chinery <philip.chinery@pdfforge.org>
2016-03-20 17:09:54 +0100

    Add test compiler project

---
+++ CompilationTestSolution/Library2/StringAmender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library2
+{
+    public static class StringAmender
+    {
+        public static string Amend(string s) => $"{s} 2016";
+    }
+}
---
+++ CompilationTestSolution/TestSolution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library1;
+using Library2;
+
+namespace TestSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringAmender.Amend(StringGenerator.Create()));
+        }
+    }
+}
